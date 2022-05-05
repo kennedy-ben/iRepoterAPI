e4f178b1c5168899bb5c9a923d927dc291793c81
kennedy bernard <makuanikennedy@gmail.com>
2022-05-05 14:25:43 +0300

    transfered API url from top to bottom

--- polls/urls.py
+++ polls/urls.py
@@ -9,9 +9,11 @@ router.register(r'people', PersonViewSet)
 router.register(r'species', SpeciesViewSet)
 
 urlpatterns = [
-   path('', include(router.urls)),
-   # path('', views.home, name='home'),
+#    path('', include(router.urls)),
+   path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
+    path('', include(router.urls)),
 ]
+
